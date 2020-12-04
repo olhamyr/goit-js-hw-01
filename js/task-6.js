221b28f234c825a3bce89c954bb56b4efb773b97
Olha Myroshnychenko <olhamyr001@gmail.com>
2020-12-04 13:32:27 +0200

    performs the task6

---
+++ js/task-6.js
@@ -0,0 +1,15 @@
+let total = 0;
+
+do {
+    let input = prompt('Введите число');
+
+  if (input === null) {
+      break;
+  }  
+
+  input = Number(input);
+  total += input;
+
+} while (true);
+
+alert(`Общая сумма чисел равна ${total}`);
