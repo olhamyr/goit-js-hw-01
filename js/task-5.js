33320cfc2d2f3c54a6747dbff7565728b205324d
Olha Myroshnychenko <olhamyr001@gmail.com>
2020-12-05 14:56:31 +0200

    fixes bugs in task-5

--- js/task-5.js
+++ js/task-5.js
@@ -7,34 +7,39 @@ let country;
 
 
 let deliveryCountry = prompt('Введите страну');
-deliveryCountry = deliveryCountry.toLowerCase();
 
-switch (deliveryCountry) {
-    case CHINA:
-    country='Китай'    
-    console.log(`Доставка в ${country} будет стоить 100 кредитов`);
-        break;
+if (deliveryCountry === null) { console.log('Пользователь отменил ввод'); }
 
-        case CHILIE:
-    country='Чили'    
-    console.log(`Доставка в ${country} будет стоить 250 кредитов`);
-        break;
+else {deliveryCountry = deliveryCountry.toLowerCase(); 
 
-        case AUSTRALIA:
-    country='Австралия'    
-    console.log(`Доставка в ${country} будет стоить 170 кредитов`);
-        break;
-
-        case INDIA:
-    country='Индия'    
-    console.log(`Доставка в ${country} будет стоить 180 кредитов`);
-        break;
-
-        case JAMAICA:
-    country='Ямайка'    
-    console.log(`Доставка в ${country} будет стоить 120 кредитов`);
-        break;
-
-    default:
-        alert('В вашей стране доставка не доступна');
+    switch (deliveryCountry) {
+        case CHINA:
+        country='Китай'    
+        console.log(`Доставка в ${country} будет стоить 100 кредитов`);
+            break;
+    
+            case CHILIE:
+        country='Чили'    
+        console.log(`Доставка в ${country} будет стоить 250 кредитов`);
+            break;
+    
+            case AUSTRALIA:
+        country='Австралия'    
+        console.log(`Доставка в ${country} будет стоить 170 кредитов`);
+            break;
+    
+            case INDIA:
+        country='Индия'    
+        console.log(`Доставка в ${country} будет стоить 180 кредитов`);
+            break;
+    
+            case JAMAICA:
+        country='Ямайка'    
+        console.log(`Доставка в ${country} будет стоить 120 кредитов`);
+            break;
+    
+        default:
+            alert('В вашей стране доставка не доступна');
+    }
 }
+
